27c28daccd96db235d543c74cc8452cbd289195f
reusent <camilogaudi@gmail.com>
2022-05-19 15:42:47 -0500

    First commit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,6 +5,15 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  if (x>y){
+    return x
+  }
+  else if (x<y){
+    return y
+  }
+  else{
+    return x
+  }
 }
 
 function mayoriaDeEdad(edad) {
